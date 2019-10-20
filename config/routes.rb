77e1bce5cf7d25d42a7d7fe3af0f22ef9dae95db
reiko watanabe <blackgothy@gmail.com>
2019-10-20 21:08:45 +0900

    Add email registration for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'diaries#index'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :diaries
 
   if Rails.env.development?
--- db/schema.rb
+++ db/schema.rb
@@ -36,11 +36,16 @@ ActiveRecord::Schema.define(version: 2019_10_20_105503) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "name", null: false
     t.string "avatar"
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
