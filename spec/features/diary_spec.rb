4ab29c56acbe15614e62b823478216c97c2633fb
reiko watanabe <blackgothy@gmail.com>
2019-10-20 19:32:36 +0900

    Add rspec config

---
+++ spec/features/diary_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "日記作成機能", type: :feature do
+  background do
+    FactoryBot.create(:diary)
+  end
+
+  scenario "日記一覧のテスト" do
+    visit diaries_path
+
+    expect(page).to have_content '本文'
+  end
+
+  scenario "日記作成のテスト" do
+    visit new_diary_path
+
+    fill_in 'diary_title', with: '日記2'
+    fill_in 'diary_body', with: '本文2'
+    fill_in 'diary_worked_date', with: '2019/10/19'
+    select 3, :from => 'diary_rank'
+
+    click_on '登録する'
+    expect(page).to have_content '本文2'
+  end
+
+end
--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a worked_date, rank" do
+    diary = Diary.new(
+      title: 'テスト',
+      worked_date: nil,
+      body: '本文',
+      rank: 5,
+      image: nil)
+    expect(diary).not_to be_valid
+  end
 end
