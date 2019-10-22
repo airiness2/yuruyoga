36376d618ac443eab5e42a3079363d6e2949c3fd
reiko watanabe <blackgothy@gmail.com>
2019-10-22 22:01:45 +0900

    Add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'diaries#index'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  resources :users, :only => [:show]
 
   resources :diaries
 
