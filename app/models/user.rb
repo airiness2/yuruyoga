ccc76db72a1c1ac84a9bf8e003165be69b2634b9
reiko watanabe <blackgothy@gmail.com>
2019-11-06 15:10:05 +0900

    Add association to user and relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :diaries, dependent: :destroy
+
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   mount_uploader :avatar, AvatarUploader
 end
