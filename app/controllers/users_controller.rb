a6d842dca09b6cba607301fede352d20c8612ba5
reiko watanabe <blackgothy@gmail.com>
2019-11-06 14:54:53 +0900

    Add message link to users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
-  def show;end
+  def show
+    if @user == current_user
+      render 'mypage'
+    end
+  end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :avatar, :admin)
   end
-  
+
   def set_user
     @user = User.find(params[:id])
   end
