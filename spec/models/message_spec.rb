aa4aab2ba0f48a6b29c3e06fba5f91478e2d1a78
reiko watanabe <blackgothy@gmail.com>
2019-11-13 14:27:16 +0900

    Fix model test

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  it "is valid with a name" do
-    conversation = FactoryBot.create(:conversation)
+  it "is valid with a body" do
+    user = FactoryBot.create(:user, email: "test100@example.com")
+    user2 = FactoryBot.create(:user, email: "test101@example.com")
+    conversation = Conversation.create(
+      sender_id: user.id,
+      recipient_id: user2.id)
     message = conversation.messages.build(
-      user_id: "2",
+      user_id: user2.id,
       conversation_id: "1",
       read: true,
       body: "メッセージ"
