8a7615c5813ac568cdcca2fa213a25aaca57291c
reiko watanabe <blackgothy@gmail.com>
2019-12-01 14:47:03 +0900

    Add show controller to poses

--- app/controllers/poses_controller.rb
+++ app/controllers/poses_controller.rb
@@ -1,9 +1,13 @@
 class PosesController < ApplicationController
+  before_action :set_pose, only: [:show]
+
   def index
     @q = Pose.ransack(params[:q])
     @poses = @q.result.order(created_at: :desc)
     @poses = @poses.page(params[:page]).per(10)
   end
+
+  def show; end
 
   private
 
