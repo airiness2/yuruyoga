17fb69d5225a024550e1e755e662f92edc0f000f
reiko watanabe <blackgothy@gmail.com>
2019-12-08 08:15:00 +0900

    Add delete function for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,17 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to diary_path(@diary) }
       else
-        format.html { redirect_to diary_path(@diary), notice: '投稿できませんでした...' }
+        format.html { redirect_to diary_path(@diary), notice: 'コメントを投稿できませんでした。' }
       end
+    end
+  end
+
+  def destroy
+    @diary = Diary.find(params[:diary_id])
+    @comment = Comment.find(params[:id])
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      redirect_to diary_path(@diary), notice: "日記を削除しました！"
     end
   end
 
