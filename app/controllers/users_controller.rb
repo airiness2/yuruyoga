5f2b25f6aaf0254c2ede90cefae54e1c9b5d5d07
reiko watanabe <blackgothy@gmail.com>
2019-12-07 13:01:36 +0900

    Add edit controller for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show;end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "プロフィールを更新しました！"
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :avatar, :admin)
+    params.require(:user).permit(:name, :email, :avatar, :remove_avatar)
   end
 
   def set_user
