2ca4934b08a29282efa252a1c4cdf6294284605e
reiko watanabe <blackgothy@gmail.com>
2019-12-11 16:11:13 +0900

    Add admin_request controller

---
+++ app/controllers/admin/requests_controller.rb
@@ -0,0 +1,35 @@
+class Admin::RequestsController < ApplicationController
+  before_action :set_request, only: [:show, :edit, :update, :destroy]
+  before_action :admin_flg
+
+  def index
+    @requests = Request.all.page(params[:page]).per(10)
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @request.update(request_params)
+      redirect_to admin_request_path, notice: "要望を編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @request.destroy
+    redirect_to admin_requests_path, notice: "要望を削除しました！"
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:body, :status, :user_id)
+  end
+
+  def set_request
+    @request = Request.find(params[:id])
+  end
+end
