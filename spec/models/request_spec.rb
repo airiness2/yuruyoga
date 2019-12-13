501246fef15410231081e94a2c9d202bb1097247
reiko watanabe <blackgothy@gmail.com>
2019-12-13 15:38:12 +0900

    Add model spec for request

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Request, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a body" do
+    user = FactoryBot.create(:user, email: "request@example.com")
+    request = Request.new(
+      status: 1,
+      body: 'タグ1',
+      user_id: user.id
+      )
+    expect(request).to be_valid
+  end
 end
