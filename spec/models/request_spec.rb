e9e9eb6aaa74fc87894c8a801ba99cc1e1606274
reiko watanabe <blackgothy@gmail.com>
2019-12-16 16:09:16 +0900

    Add system spec for request

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Request, type: :model do
     user = FactoryBot.create(:user, email: "request@example.com")
     request = Request.new(
       status: 1,
-      body: 'タグ1',
+      body: '要望1',
       user_id: user.id
       )
     expect(request).to be_valid
