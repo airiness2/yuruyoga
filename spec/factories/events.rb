9cd52e8bf26dacbeb6017294b4ef71986bbae43e
reiko watanabe <blackgothy@gmail.com>
2019-12-24 16:32:33 +0900

    Fix test for events

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :event do
     name { "イベント1" }
-    hold_date { "2019-09-29 13:00:00" }
+    hold_date { Time.now.tomorrow.to_date }
     place { "渋谷" }
     url { "http://localhost" }
     detail { "イベント詳細1" }
