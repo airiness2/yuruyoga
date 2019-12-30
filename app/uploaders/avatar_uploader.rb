b17d5f1c83900e72a34c152f5843776a9585fee4
reiko watanabe <blackgothy@gmail.com>
2019-12-30 18:58:20 +0900

    Fix uploader settings

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,15 +2,17 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
 
+  include CarrierWave::MiniMagick
+
   if Rails.env.production?
     include Cloudinary::CarrierWave
   else
-  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
 
+end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -48,6 +50,5 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-end
   process resize_to_limit: [300, 300]
 end
