dc9c1579dbfc2ef7e393000d75898db9791c3d0a
reiko watanabe <blackgothy@gmail.com>
2020-01-21 06:12:27 +0900

    Fix uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     include Cloudinary::CarrierWave
     CarrierWave.configure do |config|
       config.cache_storage = :file
+      config.user_filename = true
     end
   else
     include CarrierWave::MiniMagick
@@ -20,9 +21,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def cache_dir
-    "/tmp/image"
-  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
