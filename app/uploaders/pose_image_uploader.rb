07f614bd249068c4616f703746bda6d0115c3c8d
reiko watanabe <blackgothy@gmail.com>
2020-02-02 10:05:48 +0900

    Add: cache dir for uploaders

--- app/uploaders/pose_image_uploader.rb
+++ app/uploaders/pose_image_uploader.rb
@@ -20,6 +20,10 @@ class PoseImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "uploads/cache/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -52,7 +56,4 @@ class PoseImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
   process :resize_to_fit => [300, 300]
-  version :thumb do
-    process :resize_to_fit => [400, 400]
-  end
 end
