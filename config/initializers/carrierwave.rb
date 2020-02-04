abb5ad20a756bd55ab8b83d18d47d6f31035b279
reiko watanabe <blackgothy@gmail.com>
2020-02-04 15:30:07 +0900

    Fix: cache settings for cloudinary

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,3 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-    config.cache_storage = :file
-  else
-    config.storage = :file
-  end
+  config.cache_storage = :file
 end
